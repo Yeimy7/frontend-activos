17e2e124dd879867bb561ca70dfd7cc9b61b55a2
Yeim <brielalica@gmail.com>
2022-10-21 10:39:34 -0400

    no desaparecer los links del navbar en modo dispositivo

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -21,24 +21,26 @@ export const Navbar = () => {
       className={`navbar navbar-expand-lg  p-0 ${!extended ? 'nav-close' : ''}`}
     >
       <div className="container-fluid py-3">
-        <button className="btn p-1 me-3" onClick={toogleMenu}>
-          <FaBars className="m-1" />
-        </button>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" to="/homeApp">
-                Home
-              </Link>
-            </li>
-          </ul>
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <button className="btn btn-nav-link" onClick={() => logout()}>
-                Cerrar Sesión
-              </button>
-            </li>
-          </ul>
+        <div className="d-flex w-100">
+          <button className="btn p-1 me-3" onClick={toogleMenu}>
+            <FaBars className="m-1" />
+          </button>
+          <div className="navContainer">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" to="/homeApp">
+                  Home
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <button className="btn btn-nav-link" onClick={() => logout()}>
+                  Cerrar Sesión
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
